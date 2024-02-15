228d4e112614b62ddaab3600923d7f14bfbe784c
Jiří H. <jiri@bacehub.de>
2024-02-15 23:53:57 +0100

    disable unused features

--- keymaps/secretpocketcat/rules.mk
+++ keymaps/secretpocketcat/rules.mk
@@ -1,46 +1,31 @@
 # Bootloader selection
 BOOTLOADER = caterina
 
-OLED_ENABLE = yes
-OLED_DRIVER_ENABLE = yes
-
+OLED_ENABLE = no
+OLED_DRIVER_ENABLE = no
 ENCODER_ENABLE = no
-
-EXTRAKEY_ENABLE = yes
 AUDIO_ENABLE = no
 MUSIC_ENABLE = no
-MOUSEKEY_ENABLE = yes
-
+MOUSEKEY_ENABLE = no
 DYNAMIC_MACRO_ENABLE = no
 COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
-
-#HAPTIC FEEBACK
 HAPTIC_ENABLE = no
-
-#PER KEY RGB
 RGB_MATRIX_ENABLE = no
-
-UNICODEMAP_ENABLE = yes
-CAPS_WORD_ENABLE = yes
-
-# Mouse
 MOUSEKEY_ENABLE = no
-
-# Space cadet
-SPACE_CADET_ENABLE = yes
 COMMAND_ENABLE = no
-
-#Autoshift
 AUTO_SHIFT_ENABLE = no
 
+EXTRAKEY_ENABLE = yes # Audio control and System control
+UNICODEMAP_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+SPACE_CADET_ENABLE = yes
+OS_DETECTION_ENABLE = yes
+
 #HID/Virtual Serial
-CONSOLE_ENABLE = yes
+CONSOLE_ENABLE = no
 RAW_ENABLE = no
 VIRTSER_ENABLE = no
-
-# OS detection
-OS_DETECTION_ENABLE = yes
 
 #Extras
 SRC += features/layer_lock.c
